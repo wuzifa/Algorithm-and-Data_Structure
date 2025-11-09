364fa42742cac422eaf67acaf2c7870a101779eb
wzf <18321504239@163.com>
2025-11-09 12:05:54 +0800

    solve a dp problem -- p1113 slove a string problem -- p2799

---
+++ Basic/p2799.cpp
@@ -0,0 +1,42 @@
+﻿#ifdef RUN_P2799
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// 每次判断后面半段经过翻转后和前面半段相不相等，相等则继续对前面半段进行同样的判断，
+// 直到后面半段和前面半段不相等，这时候就找出了答案
+int min_serach(string& str, int end)
+{
+	// 下面这句是为了两种特殊情况:
+	// 1.已经检查到只剩一个字母(end等于0了，符合下面条件)
+	// 2.检查当前字母段时有奇数个字母(end为偶数)，此时这段字母与必定不是镜子照出来的
+	// 因为镜子找出来的字母段是原先的字母段翻倍，新的字母段字母个数必定是偶数
+	if (end % 2 == 0)
+		return (end + 1);
+	int post_start, half_len;
+	// 原先在这里有下面这样一段：
+	// if (str.substr(post_start, half_len) == str.substr(0, half_len))
+	// 	return min_serach(str, post_start - 1);
+	// 这样一段会误判当前字母段为"镜子找出来的"，的确在经过镜子两次照射后后面镜子新生成的字母段因为
+	// 生成它的字母段已经是左右镜像对称的而"新字母段"和"生成它的字母段"在新字母段不reverse的情况下直接就相等
+	// 但是直接相等是照射两次后"为镜子找出来的"必要非充分条件，例如如果出现: "ABCDABCD"这样的字母段就会有漏网之鱼
+	// 所以还是老老实实地先反转再判断相不相等，这个是充要条件
+	post_start = half_len = (end + 1) / 2;
+	string reversed = str.substr(post_start, half_len);
+	std::reverse(reversed.begin(), reversed.end());
+	if (str.substr(0, half_len) != reversed)
+		return (end + 1);
+	else
+		return min_serach(str, post_start - 1);
+		
+}
+
+int main()
+{
+	string necklace;
+	cin >> necklace;
+	cout << min_serach(necklace, necklace.size() - 1) << endl;
+}
+
+#endif
---
+++ algorithm/p1113.cpp
@@ -0,0 +1,76 @@
+﻿#ifdef RUN_P1113
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> cost;//记忆化
+vector<short> chore_time;//记录题目输入以及在动态规划时的算法要用
+vector<vector<short>> prev_chore;//帮助组织动态规划算法
+
+// 这个题目子问题和子问题之间的关系很明确了
+// 完成一个杂物至少需要的时间是完成所有前置必须杂物至少所需时间的最大值加上完成当前杂物所需时间，因为状态转移的方向不是很明确 / 固定，所以用递归，不用tabulation / recursion
+// 又因为在递归的时候可能会对一个杂物反复求完成它至少所需时间，所以又要加记忆化
+// 个人感觉应该从所需前置杂物少的杂物开始向所需前置杂物多的递归，这样能体现从小问题向大问题过渡
+
+// 具体思路: 
+// 遍历图图上每个节点，求完成这个点代表的杂务至少需要的时间，求出来后记录到数组里，
+// 然后遍历完图上每个点后求每个杂务所需时间最大的 
+// 在求完成每个杂务至少需要的时间时采用递归方法，求它完成它前面必须完成的杂务中所需时间最多的，加上它自己所需的时间
+
+//原来的思路(贪心)不对(见提交记录)，因为题目的输入并不能确保图的阶段性是很明确的，在取next_chore最大的时，可能取到了跳了很多杂务的点
+
+//状态转移公式: cost(i) = max{cost(j)} + chore_time(i) j为完成i的前提杂务
+
+static int findcost(int no)
+{
+	//查询记忆化表
+	if (cost[no] != 0)
+		return cost[no];
+	else
+	{
+		int max_time = -1, new_time;
+		//针对每个前提杂务进行递归
+		for (int i : prev_chore[no])
+		{
+			new_time = findcost(i);
+			if (max_time < findcost(i))
+				max_time = new_time;
+		}
+		//将结果记录到记忆化表中，方便下次递归到这个点时不用重复计算
+		cost[no] = max_time + chore_time[no];
+		return cost[no];
+	}
+}
+
+int main()
+{
+	int chore_num;
+	cin >> chore_num;
+	cost = std::move(vector<int>(chore_num + 1, 0));
+	chore_time = std::move(vector<short>(chore_num + 1, 0));
+	prev_chore = std::move(vector<vector<short>>(chore_num + 1));
+	int chore_no, time, prev;
+	for(int i = 0; i < chore_num; i++)
+	{
+		cin >> chore_no >> time;
+		chore_time[chore_no] = time;
+		while (true)
+		{
+			cin >> prev;
+			if (prev == 0)
+				break;
+			prev_chore[chore_no].push_back(prev);
+		}
+	}
+	cost[1] = chore_time[1];
+	int max_cost = -1;
+	for (int i = 1; i <= chore_num; i++)
+	{
+		int newcost = findcost(i);
+		if (max_cost < newcost)
+			max_cost = newcost;
+	}
+	cout << max_cost << endl;
+}
+
+
+#endif
