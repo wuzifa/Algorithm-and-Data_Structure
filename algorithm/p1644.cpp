4fc189b334165058933b54491ac8d5c9b3367c17
wzf <18321504239@163.com>
2025-11-10 23:05:55 +0800

    solve a classic dp problem  (algorithm-p2842(coin change)) 解决一道递推问题 (algorithm - p1644)

---
+++ algorithm/p1644.cpp
@@ -0,0 +1,43 @@
+﻿#ifdef RUN_P1644
+#include <iostream>
+#include <vector>
+using namespace std;
+
+static vector<vector<unsigned int>> state_table;
+static vector<vector<bool>> visited;
+static int row_max;
+//访问滚动数组的一列时可能行下表大于5，导致dp表被污染
+//实际可以直接建一个数组，这个数组的空间利用率不会很低，并且行数和列数也不是很大，完全可接受
+
+// 利用递归自然递推，state_table[x][y] = state_table[x - 1][y - 2] + state_table[x - 2][y - 1] + state_table[x + 1][y - 2] + state_table[x + 2][y - 1]
+// (后面加上的前提是那个下标代表的点在棋盘里面)
+// 不要直接DFS求解原点可以到达的所有点的路径数，这样会计算大量无法到达右上角的点的路径数，浪费时间
+unsigned int dfs_table(int row, int column)
+{
+	if (column == 0 && row == 0)
+		return 1;
+	//记忆化
+	if (visited[row][column])
+		return state_table[row][column];
+	if (row + 2 <= row_max && column - 1 >= 0)
+		state_table[row][column] += dfs_table(row + 2, column - 1);
+	if (row + 1 <= row_max && column - 2 >= 0)
+		state_table[row][column] += dfs_table(row + 1, column - 2);
+	if (row - 1 >= 0 && column - 2 >= 0)
+		state_table[row][column] += dfs_table(row - 1, column - 2);
+	if (row - 2 >= 0 && column - 1 >= 0)
+		state_table[row][column] += dfs_table(row - 2, column - 1);
+	visited[row][column] = true;
+	return state_table[row][column];
+}
+
+int main()
+{
+	int column;
+	cin >> row_max >> column;
+	visited = std::move(vector<vector<bool>>(row_max + 1, vector<bool>(column + 1, false)));
+	state_table = std::move(vector<vector<unsigned int>>(row_max + 1, vector<unsigned int>(column + 1, 0)));
+	cout << dfs_table(row_max, column) << endl;
+}
+
+#endif
---
+++ algorithm/p2842.cpp
@@ -0,0 +1,42 @@
+﻿#ifdef RUN_P2842
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+// 建立dp表,每个位置表示凑出这个位置的金额所需的最小硬币数(子问题)
+// dp[i] = min{ 1 + dp[i - cashⁱ] }(cashⁱ < i)			子问题间关系，每一步尝试所有可能，取最优解
+int main()
+{
+	int cash_kind, total_num;
+	cin >> cash_kind >> total_num;
+	vector<int> cash(cash_kind, 0);
+	for (int i = 0; i < cash_kind; i++)
+	{
+		cin >> cash[i];
+	}
+	vector<int> dp(total_num + 1, 0);
+	dp[0] = 0;
+	for (int i = 1; i <= total_num; i++)
+	{
+		int min_num = INT_MAX;
+		for (int j : cash)
+		{
+			//后面这句dp[i - j] != INT_MAX一定要加上，可能出现某一个dp[i]因为
+			//所有面额都大于i而导致这个dp[i]为INT_MAX 或者 在尝试了所有的cash后所有的子状态都是INT_MAX(凑不出这样的金额)
+			//动态规划不仅会把最优解进行传递，还会把"无效状态"进行传递，如果部分子状态是无效状态(凑不出这样的金额)，那下面的机制就会自然跳过这样的情况
+			//只选取在符合"现实情况下"的最优效子状态
+			//如果所有子状态都是无效状态，那当前状态也是无效状态
+			if (j <= i && dp[i - j] != INT_MAX)
+				min_num = min(min_num, 1 + dp[i - j]);
+		}
+		dp[i] = min_num;
+	}
+	if (dp[total_num] != INT_MAX)
+		cout << dp[total_num] << endl;
+	else
+		cout << "凑不出" << endl;
+}
+
+#endif
